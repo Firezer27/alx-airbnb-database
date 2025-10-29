3b1e3e4694c01d04d4c64f1e6a3ef99663e4409c
Firezer Bizuwork <firezer.bizuwork27@gmail.com>
2025-10-29 22:46:46 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,38 @@
+-- ==============================================================
+-- 1. Non-Correlated Subquery
+-- Objective:
+--   Find all properties where the average rating is greater than 4.0
+-- ==============================================================
+Select * 
+from
+  Property As p
+where
+  p.property_id In (
+      Select
+         r.property_id
+      from 
+         Raview As r
+      group by
+         r.property_id
+      having
+         AVG(r.rating) > 4.0 
+  ) ;
+
+-- ==============================================================
+-- 2. Correlated Subquery
+-- Objective:
+--   Find users who have made more than 3 bookings.
+-- ==============================================================
+select * 
+from 
+  Users as u
+  where(
+    select
+       count(*)
+    from 
+       Booking as b
+    where 
+       b.user_id = u.user_id
+  )>3;
+
 
