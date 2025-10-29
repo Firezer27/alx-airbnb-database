6d297c3a0f44d57259b03d68ed090bf9122f3535
Firezer Bizuwork <firezer.bizuwork27@gmail.com>
2025-10-29 18:37:34 +0300

    Add SQL JOIN operations examples
    This script demonstrates various SQL JOIN operations including INNER JOIN, LEFT JOIN, and FULL OUTER JOIN.

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,32 @@
+-- ==============================================================
+-- FILE: joins_queries.sql
+-- PROJECT: ALX Airbnb Database
+-- AUTHOR: Firezer Bizuwork
+-- DESCRIPTION:
+--   This script demonstrates SQL JOIN operations:
+--     1. INNER JOIN
+--     2. LEFT JOIN
+--     3. FULL OUTER JOIN
+-- ==============================================================
 
+
+-- ==============================================================
+-- 1. INNER JOIN
+-- ==============================================================
+select * from Booking
+inner join User
+on Booking.user_id = User.user_id;
+
+-- ==============================================================
+-- 2. LEFT JOIN
+-- ==============================================================
+select * from Property
+left join Review 
+on Property.property_id = Review.property_id;
+
+-- ==============================================================
+-- 3. FULL OUTER JOIN
+-- ==============================================================
+select * from User
+full outer join Booking
+on  User.user_id = Booking.user_id;
