3d8fabba799d2ee3c89f1ecfaede931d2b88f4d9
Firezer Bizuwork <firezer.bizuwork27@gmail.com>
2025-10-30 11:42:41 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,18 @@
-
+-- ==============================================================
+-- 1. Aggregation Query: Count total bookings made by each user
+-- ==============================================================
+select
+  u.user_id,
+  u.first_name,
+  u.last_name, 
+  count(b.booking_id) as total_bookings
+from 
+  users as u
+left join
+  Booking as b
+on 
+  u.user_id = b.user_id
+group by 
+  u.user_id
+order by
+  total_bookings desc;
