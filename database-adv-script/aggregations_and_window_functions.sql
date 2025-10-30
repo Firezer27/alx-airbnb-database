650c2598ad0027bef272b18491e3f6feb979343b
Firezer Bizuwork <firezer.bizuwork27@gmail.com>
2025-10-30 13:21:02 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,20 +2,21 @@
 -- 1. Aggregation Query: Count total bookings made by each user
 -- ==============================================================
 SELECT
-  u.user_id,
-  u.first_name,
-  u.last_name, 
-  COUNT(b.booking_id) AS total_bookings
-FROM
-  users AS u
-LEFT JOIN
-  Booking AS b
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM 
+    Properties AS p
+LEFT JOIN 
+    Bookings AS b
 ON 
-  u.user_id = b.user_id
+    p.property_id = b.property_id
 GROUP BY 
-  u.user_id
-ORDER BY
-  total_bookings DESC;
+    p.property_id, p.title
+ORDER BY 
+    total_bookings DESC;
 
 
 -- ==============================================================
